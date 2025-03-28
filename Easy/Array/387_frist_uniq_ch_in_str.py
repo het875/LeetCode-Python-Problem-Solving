93bfb7b00fa75e7891f899f5e0a275b31bb9ee36
het875 <hetglathiya875@gmail.com>
2025-03-28 21:44:15 +0530

    added new files

---
+++ Easy/Array/387_frist_uniq_ch_in_str.py
@@ -0,0 +1,19 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        d = {}
+
+        for i in s:
+            if i in d:
+                d[i] += 1 
+            else:
+                d[i] = 1 
+        
+        #print(d)
+
+        
+        for i in range(len(s)):
+            if d[s[i]] == 1:
+                #print(i)
+                return i
+
+        return -1
